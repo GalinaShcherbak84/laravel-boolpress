73a9198777b6bdcb3a2cb1a382a42df5d6a9883f
GalinaSherbak84 <galinasherbak@gmail.com>
2021-06-24 21:12:13 +0200

    print posts in home

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,20 +1,54 @@
 <template>
     <div>
-        <header>header</header>
+        <header>
+        <!-- <Header/> -->
+        </header>
         <main>
-            <h1>Welcome to public home</h1>
-            <p>work in progress</p>
+            <div class="container">
+                 <h1>Blog</h1>
+                 <article v-for="post in posts " :key ="post.id">
+                     <h2>{{post.title}}</h2>
+                     <div>{{post.created_at}}</div>
+                     <p>{{post.content}}</p>
+                     <a href="">Read more</a>
+                 </article>
+            </div>
         </main>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+/* import Header from "./components/Header.vue"; */
 export default {
-    name:'App'
+    name:'App',
+    data(){
+        return{
+            posts:[]
+        }
+    },
+    created(){
+        //console.log(axios);
+        this.getPosts();
+    },
+    methods:{
+        getPosts(){
+            //get posts from API
+            axios.get('http://127.0.0.1:8000/api/posts')
+            .then(res =>{
+                console.log(res.data);
+                this.posts = res.data;
+            })
+            .catch(err =>{
+                console.log(err);
+            })
+        }
+    }
 }
 </script>
 
 <style lang="scss">
+@import '../sass/frontoffice/utilities';
     body{
         font-family: sans-serif;
     }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/* Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+Route::get('/test', function(){
+    return response()->json([
+        'names' => ['chiara', 'luca']
+    ]);
+});
+
+//get blog posts
+Route::namespace('Api')->group(function(){
+    //get posts
+    Route::get('/posts', 'PostController@index');
 });
